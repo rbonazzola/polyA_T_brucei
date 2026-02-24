0a7759d5f184bdbd384c2f938c98ce5a4dc6a452
Matthew Russell <matthewjohnrussell@gmail.com>
2026-02-24 11:39:57 +0000

    Sort by timestep to fix monotonic tail lengths

--- preprocess.R
+++ preprocess.R
@@ -26,6 +26,9 @@ filter_reads <- function(dat, min_median_tail_t0=MIN_MEDIAN_TAIL_T0) {
 filter_non_increasing <- function(dat) {
   
   reads_medians <- dat[, .(median = median(poly_tail_length)), by = .(alignment_genome, barcode)]
+
+  setorder(reads_medians, alignment_genome, barcode)
+
   nonincreasing_ids <- reads_medians[, {
     inc_pos <- which(diff(median) > 0)[1]
     if (is.na(inc_pos)) {
